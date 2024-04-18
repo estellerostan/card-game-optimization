caf2564088552481d27dd15218b9c74951a40587
Estelle Rostan <estellerostan@pm.me>
2024-04-18 07:26:17 +0200

    Do not add the same card more than two times
    It is a costly operation so find a way to do better (110ms instead of 60ms so nearly x2...)?

--- src/Deck.h
+++ src/Deck.h
@@ -13,8 +13,17 @@ struct Deck
 	Deck(std::vector<Card> setListCards, int player) {
 		for (size_t i = 0; i < 30; i++)
 		{
-			// TODO: not more than twice of the same card
-			cards.push_back(setListCards[rand() % setListCards.size()]);
+			// Not more than twice of the same card
+			bool dup = true;
+			while (dup) {
+				Card res = setListCards[rand() % setListCards.size()];
+				std::vector<Card> bar;
+				std::copy_if(cards.begin(), cards.end(), std::back_inserter(bar), [res](Card c) {return c == res; });
+				if (bar.size() < 2) {
+					cards.push_back(res);
+					dup = false;
+				}
+			}
 		}
 	}
 };
