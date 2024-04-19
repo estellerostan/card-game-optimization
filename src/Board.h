267d27bdeb7bef9f0c2310e3149a151a3d3c2db7
Estelle Rostan <estellerostan@pm.me>
2024-04-19 15:49:04 +0200

    WIP taunt

--- src/Board.h
+++ src/Board.h
@@ -17,14 +17,58 @@ struct Board
 		cards.push_back(card);
 	}
 
-	void playCards(Player* enemy) {
-		for each (Card card in cards)
-		{
-			enemy->PV = std::max(enemy->PV - card.ATK, 0);
+	void playCards(Player* enemy, Board enemyBoard) {
+		std::vector<Card*> enemyCards;
 
-			if (enemy->PV == 0) {
-				enemy->isDead = true;
-				return;
+		for each (Card c in enemyBoard.cards)
+		{
+			if (c.hasTaunt) {
+				enemyCards.push_back(new Card(c.ATK, c.DEF, c.hasTaunt, c.hasTrample, c.hasDistortion, c.hasFirstStrike));
+			}
+		}
+
+		std::vector<Card*> myCards;
+		for each (Card c in cards)
+		{
+			if (c.ATK > 0) {
+				myCards.push_back(new Card(c.ATK, c.DEF, c.hasTaunt, c.hasTrample, c.hasDistortion, c.hasFirstStrike));
+			}
+		}
+
+		for each (Card * card in myCards)
+		{
+			auto canDefend = [](Card * c) { return c->DEF > 0; };
+			if (auto it = std::find_if(std::begin(enemyCards), std::end(enemyCards), canDefend); it != std::end(enemyCards)) {
+				// taunt
+				for each (Card * enemyCard in enemyCards)
+				{
+					// attack enemy
+					if (enemyCard->DEF > 0) {
+						enemyCard->DEF -= card->ATK;
+					}
+
+					if (enemyCard->ATK == 0) {
+						continue;
+					}
+
+					// enemy attacks back
+					if (enemyCard->DEF > 0) {
+						if (card->DEF > 0) {
+							card->DEF -= enemyCard->ATK;
+						}
+					}
+
+					// only attack once
+					break;
+				}
+			}
+			else {
+				enemy->PV = std::max(enemy->PV - card->ATK, 0);
+
+				if (enemy->PV == 0) {
+					enemy->isDead = true;
+					return;
+				}
 			}
 		}
 	}
