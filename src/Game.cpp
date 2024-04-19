244e021fd4170a4d11fdc6c53b65a4db6a500cb1
Estelle Rostan <estellerostan@pm.me>
2024-04-19 07:32:49 +0200

    Fix graph 1 to only show win rates that go up

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include "Game.h"
 #include <iostream>
+#include <fstream>
 #include "Deck.h"
 #include "SetList.h"
 #include "Player.h"
@@ -8,8 +9,8 @@
 
 Game::Game()
 {
-	window_.create(sf::VideoMode(1920u, 1080u), "Card game");
-	window_.setFramerateLimit(60);
+	//window_.create(sf::VideoMode(1920u, 1080u), "Card game");
+	//window_.setFramerateLimit(60);
 
 	sf::Clock clock;
 
@@ -17,14 +18,13 @@ Game::Game()
 
 	SetList setList;
 	int referenceWinRate = 0;
-	std::vector<Card> referenceDeck;
 	std::vector<Card> currentDeckP0;
 
 	Player p1 = Player();
 	p1.createDeck(setList.cards, 1, true);
 	std::vector<Card> currentDeckP1 = p1.deck.cards;
 
-	for (size_t j = 0; j < 250; j++)
+	for (size_t j = 0; j < 500; j++)
 	{
 		Player p0;
 		if (referenceDeck.size() > 0) {
@@ -35,12 +35,16 @@ Game::Game()
 			p0 = Player();
 			p0.createDeck(setList.cards, 0, true);
 		}
+
 		currentDeckP0 = p0.deck.cards;
+		p0.addDeck(currentDeckP0, setList.cards);
+		currentDeckP0.clear();
+		std::copy(p0.deck.cards.begin(), p0.deck.cards.end(), std::back_inserter(currentDeckP0));
 
 		int currentWinRate = 0;
 		for (size_t i = 0; i < 1000; i++)
 		{
-			p0.addDeck(currentDeckP0, setList.cards);
+			p0.deck.cards = currentDeckP0;
 			p0.resetPlayer();
 			p1.addDeck(currentDeckP1);
 			p1.resetPlayer();
@@ -63,15 +67,10 @@ Game::Game()
 		if (currentWinRate > referenceWinRate) {
 			referenceWinRate = currentWinRate;
 			referenceDeck = currentDeckP0;
-			std::cout << "turn " << j << ":" << std::endl;
-			std::cout << "WR: " << currentWinRate / 10.f << "%" << std::endl << std::endl;
-
-			if (currentWinRate > 970) {
-				break;
-			}
+			//std::cout << "turn " << j << ":" << std::endl;
+			//std::cout << "WR: " << currentWinRate / 10.f << "%" << std::endl << std::endl;
+			winRates.push_back(currentWinRate);
 		}
-
-		winRates.push_back(currentWinRate);
 
 		//std::cout << "WR P1: " << currentWinRate / 10.f << "% vs P2: " << (1000 - currentWinRate) / 10.F << "%" << std::endl;
 	}
@@ -110,24 +109,35 @@ Game::Game()
 		myfile2 << var.first << "," << var.second << "\n";
 		j++;
 	}
-}
 
 	std::ofstream myfile3;
 	myfile3.open("example3.csv");
-	myfile3 << "DurÃ©e des parties.\n";
+	myfile3 << "Durée des parties.\n";
 	myfile3 << "tours, effectif,\n";
 	int h = 0;
 	for each (auto var in turnsCount)
-{
+	{
 		myfile3 << var.first << "," << var.second << "\n";
 		h++;
-			}
-		}
-
-		sf::Time elapsed = clock.restart();
-		update(elapsed.asSeconds());
-		render();
 	}
+}
+
+void Game::run()
+{
+	//while (window_.isOpen())
+	//{
+	//	for (auto event = sf::Event{}; window_.pollEvent(event);)
+	//	{
+	//		if (event.type == sf::Event::Closed)
+	//		{
+	//			window_.close();
+	//		}
+	//	}
+
+	//	sf::Time elapsed = clock.restart();
+	//	update(elapsed.asSeconds());
+	//	render();
+	//}
 }
 
 void Game::update(float deltaTime)
@@ -137,14 +147,24 @@ void Game::update(float deltaTime)
 
 void Game::render()
 {
-	window_.clear();
-	int i = 0;
-	for each (int var in winRates)
-	{
-		line[i] = sf::Vertex(sf::Vector2f(i * 2, var * 2));
-		i++;
-	}
+	//window_.clear();
+	//int i = 0;
+	//for each (int var in winRates)
+	//{
+	//	line[i] = sf::Vertex(sf::Vector2f(i * 2, var * 2));
+	//	i++;
+	//}
 
-	window_.draw(line, 1000, sf::Lines);
-	window_.display();
+	//int j = 0;
+	//for each (auto var in costDeck)
+	//{
+	//	//costLine[j] = sf::Vertex(sf::Vector2f(var.first * 10, var.second * 10));
+	//	sf::RectangleShape rectangle(sf::Vector2f(var.first * j, var.second * j));
+	//	window_.draw(rectangle);
+	//	j++;
+	//}
+
+	//window_.draw(line, 1000, sf::Lines);
+	//window_.draw(costLine, 1000, sf::Lines);
+	//window_.display();
 }
